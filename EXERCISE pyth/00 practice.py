e69b0f5564645764c02f89a9d229ec10488dfb31
DD-8775 <shashwatsingh936@gmail.com>
2025-10-03 19:43:14 +0530

    update

--- EXERCISE pyth/00 practice.py
+++ EXERCISE pyth/00 practice.py
@@ -27,12 +27,21 @@
 #       print(i[::-1])
 
 
-name = 'shahswat'
-print(name[2:])      
-print(name[:2:-1])
-print(name[:-1])
-# print("hey",1,2,3,end="200\n" )   
+# name = 'shahswat'
+# print(name[2:])      
+# print(name[:2:-1])
+# print(name[:-1])
+# # print("hey",1,2,3,end="200\n" )   
+
+h = int(input("enter the hight of triangle: "))
+for i in range(1,h+1):
+    print(" "*(h-i),"*"*(2*i-1),sep='')
 
 
-      
-       
+n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1): 
+    for j in range(n - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
+    print()   
