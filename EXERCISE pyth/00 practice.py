d9d5f1643034e4c38acb2fbb23ea58260c3494d1
DD-8775 <shashwatsingh936@gmail.com>
2026-06-03 09:50:26 +0530

    Verify

--- EXERCISE pyth/00 practice.py
+++ EXERCISE pyth/00 practice.py
@@ -1,10 +1,18 @@
-term = int(input("Enter the no of terms in the list: "))
-a = []
-for i in range (term):
-    value = int(input("Enter the value: "))
+# term = int(input("Enter the no of terms in the list: "))
+# a = []
+# for i in range (term):
+#     value = int(input("Enter the value: "))
 
-    a.append(value)
+#     a.append(value)
 
-print(a)
+# print(a)
 
-    
+a="abcdefghijklmnopqrstuvwxyz"
+v = "aeiou"
+l=[]
+for i in a:
+    for j in v:
+        if i == j:
+            l.append(i)
+for k in l:
+    print(k,end=",")
