44976fa0b227f2202429edbee634fd84156f1595
DD-8775 <shashwatsingh936@gmail.com>
2026-06-26 18:07:24 +0530

    Added info about import

---
+++ BASICS PYTH/20 More about import .py
@@ -0,0 +1,5 @@
+from math import pi , sqrt as square_root   # Her we import pi as it is and sqrt as square_root to use it in our code.
+print(pi , square_root(16))
+
+from Welcomee import welcome, name
+welcome(name)
---
+++ BASICS PYTH/Welcomee.py
@@ -0,0 +1,5 @@
+name = "Shashwat Singh"
+
+def welcome(n):
+    print(f"Welcome {n} happy to see you here!")
+
